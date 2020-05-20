a55d819352423a86e866a97ce0aabe0650c4e197
Evandro oliveira <vandoaparecido@hotmail.com>
2020-05-19 22:24:15 -0300

    Declaração e casting dos objetos

--- app/src/main/java/com/example/covid/MainActivity.java
+++ app/src/main/java/com/example/covid/MainActivity.java
@@ -4,11 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ScrollView;
+import android.widget.TextView;
+
+import com.leo.simplearcloader.SimpleArcLoader;
 
 import org.eazegraph.lib.charts.PieChart;
 
 public class MainActivity extends AppCompatActivity {
-
+        TextView tvCases, tvRecovered,tvCritical,tvActive,tvTodayCases,tvTotalDeaths,tvAffectedConuntries;
+        SimpleArcLoader simpleArcLoader;
+        ScrollView scrollCiew;
         PieChart pieChart;
 
     @Override
@@ -16,6 +22,16 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        tvCases = findViewById(R.id.tvCases);
+        tvRecovered = findViewById(R.id.tvRecovered);
+        tvCritical = findViewById(R.id.tvCritical);
+        tvActive = findViewById(R.id.tvActive);
+        tvTodayCases = findViewById(R.id.tvTodayCases);
+        tvTotalDeaths = findViewById(R.id.tvTotalDeaths);
+        tvAffectedConuntries = findViewById(R.id.tvAffectedCountries);
+
+        simpleArcLoader = findViewById(R.id.loader);
+        scrollCiew = findViewById(R.id.scrollStats);
         pieChart = findViewById(R.id.piechart);
     }
 
